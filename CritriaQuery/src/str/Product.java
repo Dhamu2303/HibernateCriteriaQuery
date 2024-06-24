78b1927d657b330e2def9296ad44ee8b9a094144
Dhamodharan Ravichandran <162108469+Dhamu2303@users.noreply.github.com>
2024-06-24 10:28:58 +0530

    Add files via upload

---
+++ CritriaQuery/src/str/Product.java
@@ -0,0 +1,27 @@
+package str;
+
+public class Product {
+	
+	private int ProductId;
+	private String ProName;
+	private double Price;
+	public int getProductId() {
+		return ProductId;
+	}
+	public void setProductId(int productId) {
+		ProductId = productId;
+	}
+	public String getProName() {
+		return ProName;
+	}
+	public void setProName(String proName) {
+		ProName = proName;
+	}
+	public double getPrice() {
+		return Price;
+	}
+	public void setPrice(double price) {
+		Price = price;
+	}
+	
+}
